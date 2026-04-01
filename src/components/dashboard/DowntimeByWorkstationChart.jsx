b3fe5b08647db0b53577132109cf190dc394db5e
Dalton McKinney <dmckinney@Ditech.internal>
2026-04-01 14:43:36 -0400

    downtime by workstation, location filter

--- src/components/dashboard/DowntimeByWorkstationChart.jsx
+++ src/components/dashboard/DowntimeByWorkstationChart.jsx
@@ -6,29 +6,64 @@ import {
   Tooltip,
   ResponsiveContainer,
 } from "recharts";
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import groupLogsByField from "../../kpi-utils/group-by-field";
 import formatDuration from "../../kpi-utils/format-duration";
 import "./KPICard.css";
 
 export default function DowntimeByWorkstationChart({logs}) {
+    const [location, setLocation] = useState(() => 'main');
+
     const data = useMemo(() => {
         return groupLogsByField(logs, "workstation");
     }, [logs]);
+
+    const filteredData = useMemo(() => {
+        return data.filter(log => log.Location === location);
+    }, [location, data]);
+
+    console.log(filteredData);
   
     return (
-      <div className="kpi-card">
-          <h3 className="header-center">Downtime by Workstation</h3>
-          <hr />
+        <div className="kpi-card">
+            <div className="card-header">
+                <h3 className="">Downtime by Workstation</h3>
 
-          <ResponsiveContainer width="100%" height={300}>
-              <BarChart data={data}>
-                  <XAxis dataKey="Name" stroke="#e5e7eb"/>
-                  <YAxis tickFormatter={(v) => Math.round(v / 3600000) + "h"} stroke="#e5e7eb"/>
-                  <Tooltip formatter={(value) => formatDuration(value)} radius={[6, 6, 0, 0]}/>
-                  <Bar dataKey="Downtime" fill="#0d6efd" />
-              </BarChart>
-          </ResponsiveContainer>
-      </div>
+                <div className="location-filter">
+                    <label>
+                        <input
+                        type="radio"
+                        name="location"
+                        value="main"
+                        checked={location === "main"}
+                        onChange={(e) => setLocation(e.target.value)}
+                        />
+                        Main St
+                    </label>
+
+                    <label className="ms-2">
+                        <input
+                        type="radio"
+                        name="location"
+                        value="walnut"
+                        checked={location === "walnut"}
+                        onChange={(e) => setLocation(e.target.value)}
+                        />
+                        Walnut St
+                    </label>
+                </div>
+            </div>
+          
+            <hr />
+
+            <ResponsiveContainer width="100%" height={300}>
+                <BarChart data={filteredData}>
+                    <XAxis dataKey="Name" stroke="#e5e7eb"/>
+                    <YAxis tickFormatter={(v) => Math.round(v / 3600000) + "h"} stroke="#e5e7eb"/>
+                    <Tooltip formatter={(value) => formatDuration(value)} radius={[6, 6, 0, 0]}/>
+                    <Bar dataKey="Downtime" fill="#0d6efd" />
+                </BarChart>
+            </ResponsiveContainer>
+        </div>
     );
 }
