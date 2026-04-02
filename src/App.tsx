e50926f97eea2aed1aebe541e5c094fd8fe4cb8d
Dalton McKinney <dmckinney@Ditech.internal>
2026-04-02 10:48:59 -0400

    footer displayed

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,24 @@
 import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 import Dashboard from "./components/dashboard/Dashboard";
 import WorklogForm from "./components/worklog-form";
 import WorkLogHistory from "./components/worklog-history/worklog-history";
 
 function App() {
   return (
-    <div>
-      <Header />
+    <div className="app-container">
+      <div className="app-content">
+        <Header />
 
-      <Routes>
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/new-maintenance-log" element={<WorklogForm />} />
-        <Route path="/logs" element={<WorkLogHistory />} />
-      </Routes>
+        <Routes>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/new-maintenance-log" element={<WorklogForm />} />
+          <Route path="/logs" element={<WorkLogHistory />} />
+        </Routes>
+      </div>
+      
+      <Footer />
     </div>
   );
 }
