c80dcd552b681507efc446a1b342259e68e0222e
Dalton McKinney <dmckinney@Ditech.internal>
2026-04-15 11:11:07 -0400

    mobile kpi styles

--- src/components/dashboard/KPICard.jsx
+++ src/components/dashboard/KPICard.jsx
@@ -1,9 +1,9 @@
 import "./KPICard.css";
 
-export default function KPICard({title, value}) {
+export default function KPICard({title, value, optionalClass=""}) {
 
     return(
-        <div className="kpi-card">
+        <div className={`kpi-card ${optionalClass}`}>
             <div className="kpi-title">{title}</div>
             <div className="kpi-value">{value}</div>
         </div>
